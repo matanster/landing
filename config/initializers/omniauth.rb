4bb6c6df8fc951ceeed01495da1c9436aa1a81a5
matanster <matan@otozip.com>
2013-11-01 23:17:43 +0200

    Rails fully initialized, plus dailycred

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.configuration.DAILYCRED_CLIENT_ID = "b8a202e3-58b0-4abd-8d64-916856326256"
+Rails.configuration.DAILYCRED_SECRET_KEY = "41cb3248-3f57-4b2d-b5da-7d870cad46f5-be7063b4-2fb5-425f-a670-cb75449b43a7"
+
+# configure where users should be redirected after authentication
+#
+# Rails.configuration.DAILYCRED_OPTIONS = {
+#   :after_auth => '/hello', #after login
+#   :after_unauth => '/goodbye' #after logout
+# }
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landing::Application.config.secret_key_base = '03a0e84eb3b4ce8d2fbb0472d4d20e2a64bc706594a73ab19b9183a4db7490c867e86b9b6c1d673e64148c8e7d1e219395b014b05c0bef3770e353b7afcd7380'
